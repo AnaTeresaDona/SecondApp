3dc47b63a2946844d916c14394f1ada4d65acba8
AnaTeresaDona <anatdona@gmail.com>
2020-07-09 23:11:54 -0400

    First commit of second ruby/HTML ruby excercise

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,3 +1,17 @@
+
+
+
+//= require jquery.min
+//= require jquery.dropotron.min
+//= require jquery.scrolly.min
+//= require jquery.scrollex.min
+//= require browser.min
+//= require breakpoints.min
+//= require util
+//= require main
+
+
+
 //= link_tree ../images
 //= link_directory ../javascripts .js
 //= link_directory ../stylesheets .css
